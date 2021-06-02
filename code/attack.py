6ac88eff845aa743612f5f6d6ed6d4312bebc595
MXSMCI <maximilian.samsinger@mci.edu>
2021-06-02 17:03:37 +0200

    Factorized attack.py

--- code/attack.py
+++ code/attack.py
@@ -1,22 +1,33 @@
-import torch
-import torch.nn as nn
 import os
 import numpy as np
 import pandas as pd
+
+# Torch
+import torch
 from torch.utils.data import DataLoader
-from torch.optim import SGD
-from torch.optim.lr_scheduler import StepLR 
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
 
-# Custom modules
-from Networks import CIFAR_Net
-from train import train_model
-
 # Foolbox
 import foolbox as fb
-from foolbox import PyTorchModel, accuracy, samples
+from foolbox import PyTorchModel
 from foolbox.attacks import LinfPGD, FGSM, L0BrendelBethgeAttack, L2CarliniWagnerAttack
+
+def compute_confusion_matrix(model, attack, attack_kwargs, dataloader, device, save_path=None):
+    confusion_matrix = np.zeros((10,10), dtype=int)
+
+    for images, labels in dataloader:
+        images = images.to(device) # Perform Tensor device conversion
+        labels = labels.to(device)
+    
+        raw_advs, clipped_advs, success = attack(fmodel, images, labels, **attack_kwargs)
+        
+        predicted_labels = model(clipped_advs).argmax(dim=1).cpu().numpy()
+        source_labels = labels.data.cpu().numpy()
+        
+        for i, j in zip(source_labels, predicted_labels):
+            confusion_matrix[i,j] += 1
+    return confusion_matrix
 
 if __name__ == "__main__":
     save_path = "models" + os.sep + "CIFAR-10" + os.sep
@@ -45,38 +56,10 @@ if __name__ == "__main__":
     
     
     attack = LinfPGD()
-    eps = 1
+    attack_kwargs = {"epsilons": 1}
     
-    #criterion = nn.CrossEntropyLoss()
-    
-    
-    running_loss = 0.0 # keep track of loss
-    running_corrects = 0 # count of carrectly classified images
-    
-    
-    confusion_matrix = np.zeros((10,10), dtype=int)
-    
-    
-    
-    for images, labels in dataloader:
-        images = images.to(device) # Perform Tensor device conversion
-        labels = labels.to(device)
-    
-        raw_advs, clipped_advs, success = attack(fmodel, images, labels, epsilons=eps)
-        
-        predicted_labels = model(clipped_advs).argmax(dim=1)
-        
-        running_corrects += torch.sum(predicted_labels == labels.data)
-    
-        source_labels = labels.data.cpu().numpy()
-        predicted_labels = predicted_labels.cpu().numpy()
-        
-        # outputs = model(images) # forward pass through network
-        # loss = criterion(outputs, labels) # Calculate loss
-    
-        # running_loss += loss.detach() * images.size(0) # multiply mean loss by the number of elements
-        for i, j in zip(source_labels, predicted_labels):
-            confusion_matrix[i,j] += 1
+    confusion_matrix = compute_confusion_matrix(
+        model, attack, attack_kwargs, dataloader, device)
     
     # For MNIST
     names = [str(k) for k in np.arange(10)]
@@ -89,3 +72,4 @@ if __name__ == "__main__":
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
     print(df)
+    
